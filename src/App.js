b8700ab51e59f2dfaa257ad8b5f32e448bbae765
kidboi666 <70879978+kidboi666@users.noreply.github.com>
2024-02-04 11:12:46 +0900

    마무리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import Dice from './Dice.js';
-import HandButton from './HandButton.js';//핸드버튼 가져오고
+import { useState } from 'react';
+import Board from './Board';
+import Button from './Button';
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
-  const handleClick = (event) => console.log(event); // 파라미터를 콘솔로그로 뿌리는 함수 하나 선언
-  return ( // 핸드버튼 컴포넌트에 value 프롭, 온클릭 프롭 속성을 각각 추가
-    <div> 
-      <HandButton value="rock" onClick={handleClick} /> {/* 콘솔뿌리는 함수 자체를 넣음 */}
-      <HandButton value="scissor" onClick={handleClick} />
-      <HandButton value="paper" onClick={handleClick} />
+  const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
+  const [otherNum, setOtherNum] = useState(1);
+  const [otherSum, setOtherSum] = useState(0);
+  const [otherGameHistory, setOtherGameHistory] = useState([]);
+
+  function handleRollClick() {
+    const nextNum = random(6);
+    const nextOtherNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]); // spread구문으로 클릭 할때 마다 새 배열을 가리키는 참조값을 만들어 state가 값의 변동을 인지하게끔 한다.
+    // 그럼 기존의 만들어진 배열들은 모두 가비지 컬렉션으로 폐기되는건가?
+    setOtherNum(nextOtherNum);
+    setOtherSum(otherSum + nextOtherNum);
+    setOtherGameHistory([...otherGameHistory, nextOtherNum]);
+  }
+
+  function handleClearClick() {
+    setNum(1);
+    setSum(0);
+    setGameHistory([]);
+    setOtherNum(1);
+    setOtherSum(0);
+    setOtherGameHistory([]);
+  }
+
+  return (
+    <div>
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <div>
+        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory}/>
+        <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory}/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
